bc50b7d80f169653abaab53dc4b5937c58268d26
prosperemebo <emeboprosper@gmail.com>
2024-04-08 22:02:18 +0100

    New Push

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -2,7 +2,7 @@ function getCurrentYear() {
   const date = new Date();
   return date.getFullYear();
 }
-  
+
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {
     [`income-${getCurrentYear()}`]: income,
