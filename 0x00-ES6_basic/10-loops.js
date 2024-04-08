ec3ace9c8b5e5b0d58392ac903c55b04205c6108
prosperemebo <emeboprosper@gmail.com>
2024-04-08 22:08:09 +0100

    New Push

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let idx of array) {
-    const value = array[idx];
-    array[idx] = appendString + value;
+  const result = [];
+
+  for (const value of array) {
+    result.push(appendString + value);
   }
 
   return array;
