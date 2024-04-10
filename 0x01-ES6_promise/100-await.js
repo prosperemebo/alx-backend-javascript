c18798934b75a61dbe86cdbef3f3352492e12629
prosperemebo <emeboprosper@gmail.com>
2024-04-10 11:20:26 +0100

    Update 100-await.js

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -4,7 +4,6 @@ export default async function asyncUploadUser() {
   try {
     const photo = await uploadPhoto();
     const user = await createUser();
-
     return { photo, user };
   } catch (error) {
     return { photo: null, user: null };
