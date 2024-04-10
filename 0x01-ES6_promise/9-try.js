fe48b1daf8b92cb971ccfae0a0837a0883957988
prosperemebo <emeboprosper@gmail.com>
2024-04-10 10:50:22 +0100

    New push

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,10 +1,11 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-
-  mathFunction()
-    .then((result) => queue.push(result))
-    .catch((error) => queue.push(error.toString()) )
-    .finally(() => queue.push('Guardrail was processed'));
-
+  try {
+    queue.push(mathFunction());
+  } catch (error) {
+    queue.push(error.toString());
+  } finally {
+    queue.push('Guardrail was processed');
+  }
   return queue;
 }
