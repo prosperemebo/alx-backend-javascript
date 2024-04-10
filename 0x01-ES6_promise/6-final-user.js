59241940e90db459de49bc7c553e1ca1d49864a9
prosperemebo <emeboprosper@gmail.com>
2024-04-10 10:41:22 +0100

    New Push

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,16 +1,14 @@
 import signUpUser from './4-user-promise';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((results) => {
-    const result = [];
-    for (const result of results) {
-      if (result.status === 'fulfilled')
-        result.push({ status: result.status, value: result.value });
-      else result.push({ status: result.status, value: `${result.reason}` });
-    }
-    return result;
-  });
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((response) => {
+        const result = [];
+        for (const data of response) {
+          if (data.status === 'fulfilled')
+            result.push({ status: data.status, value: data.value });
+          else result.push({ status: data.status, value: `${data.reason}` });
+        }
+        return result;
+    });
 }
