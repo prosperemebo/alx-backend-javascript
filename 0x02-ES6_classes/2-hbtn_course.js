7f30f128292a6b5a440e380ab3653b55c924f041
prosperemebo <emeboprosper@gmail.com>
2024-04-11 23:04:02 +0100

    Fixed Task 3

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,19 +1,46 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') {
-      throw TypeError('Name must be a string');
+      throw new TypeError('Name must be a string');
     }
 
     if (typeof length !== 'number') {
-      throw TypeError('Length must be a number');
+      throw new TypeError('Length must be a number');
     }
 
     if (!Array.isArray(students)) {
-      throw TypeError('Students must be an array');
+      throw new TypeError('Students must be an array');
     }
 
-    _name = name;
-    _length = length;
-    _students = students;
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') this._name = name;
+    else throw new TypeError('Name must be a string');
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    if (typeof length === 'number') this._length = length;
+    else throw new TypeError('Length must be a number');
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    if (!Array.isArray(students)) this._students = students;
+    else throw new TypeError('Students must be an array');
   }
 }
