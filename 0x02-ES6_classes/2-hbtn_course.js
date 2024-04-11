71219d749937cb73b20c66c5ccb6ad946488bd45
prosperemebo <emeboprosper@gmail.com>
2024-04-11 23:29:37 +0100

    New push

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -4,10 +4,8 @@ export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name === 'string') this._name = name;
     else throw new TypeError('Name must be a string');
-
     if (typeof length === 'number') this._length = length;
     else throw new TypeError('Length must be a number');
-
 		if (
       Array.isArray(students) &&
       students.every((student) => typeof student === 'string')
@@ -44,10 +42,7 @@ export default class HolbertonCourse {
     if (
       Array.isArray(students) &&
       students.every((student) => typeof student === 'string')
-    ) {
-      this._students = students;
-    } else {
-      throw new TypeError('Students must be an array of strings');
-    }
+    ) this._students = students
+		else throw new TypeError('Students must be an array of strings');
   }
 }
---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,19 @@
+export default class Building {
+  /* eslint-disable no-underscore-dangle */
+  constructor(sqft) {
+    if (
+      this.constructor !== Building &&
+      this.evacuationWarningMessage === undefined
+    ) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage'
+      );
+    }
+
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
